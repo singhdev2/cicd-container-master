2ee7343b2639960b7e3d0073f88ec9cd7fa050e6
singhdev2 <72092735+singhdev2@users.noreply.github.com>
2020-11-18 16:44:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Amar!!"));
+app.get("/", (req, res) => res.send("Hello Ama!!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
